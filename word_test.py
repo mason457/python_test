ce8ecfc463db85d6b36b574224cb869f5c68ceda
mason457 <88136451+mason457@users.noreply.github.com>
2021-07-30 15:27:14 +0800

    Create word_test.py

---
+++ word_test.py
@@ -0,0 +1,61 @@
+#註解
+#
+print("hello word")
+#數字
+3456
+#福點數
+3.14159265
+#中文
+"我是誰"
+#英文
+"hollo world"
+#布林
+True
+False
+#字串
+#有順序 可變動 list
+[3,4,5]
+LIST1 = ["hello world",123,3.14,"我"]
+#有順序 不可變動 Tuple
+(3,4,5)
+Tuple1 = ("hello world",123,3.14,"我")
+#集合 Set
+Set1 = {"hello world",123,3.14,"我"}
+#字典
+LAV = {"apple":"蘋果","data":"資料"}
+
+#變數
+a = 5
+X = 3
+Y = 4
+print(a)
+print(3)
+print(LIST1)
+#數字運算
+b = X ** Y
+print(b)
+#字串運算
+S = "List:"
+print(S)
+print(LIST1[3])
+print(S[3])
+print("字串測試[1:]")
+print(S[1:])
+print("字串測試[:4]")
+print(S[:4])
+#list
+grade1 = [12,60,45,70,90]
+grade1[0] = 55
+#grade1[1:3] =[] #1:3不含3 設為空
+#grade1+=[12,33] #新增
+length = len(grade1)
+print(grade1)
+print(grade1[1:4])
+print(length)
+data = [[1,2,3],[4,5,6]]
+print(data[0][0])
+#tuple
+grade2 = (12,60,45,70,90)
+#grade2[0] = 55
+print(grade2)
+print(grade1)
